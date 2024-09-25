84e74a21961c8e19d8d36aa2eb169e73c2317036
Tazuddin26 <tazuddin26@gmail.com>
2024-09-26 04:40:33 +0600

    added home blog button

--- js/DonateHistory.js
+++ js/DonateHistory.js
@@ -8,14 +8,25 @@ document.getElementById("btn-history").addEventListener("click", function () {
   donateCardById("history-card", 2);
 });
 //  Blog Button Toggle ------>
-const btn = document.getElementById("btn-blog");
-btn.addEventListener("click", function () {
-  if (btn.innerText === "Blog") {
-    btn.innerText = "Home";
-    donateCardById("important-fqa");
-  } else {
-    btn.innerText = "Blog";
-    window.location.href = "/index.html";
-  }
-});
+// const btn = document.getElementById("btn-blog");
+// btn.addEventListener("click", function () {
+//   if (btn.innerText === "Blog") {
+//     btn.innerText = "Home";
+//     donateCardById("important-fqa");
+//   } else {
+//     btn.innerText = "Blog";
+//     window.location.href = "/index.html";
+//   }
+// });
 
+let isBtn = true;
+document.getElementById('btn-blog').addEventListener('click', function () {
+  if (isBtn) {
+    donateCardById("important-fqa");
+    document.getElementById('btn-blog').innerText = "Home";
+  } else {
+    window.location.href = '/index.html';
+     document.getElementById("btn-blog").innerText = "Blog";
+  }
+  isBtn = !isBtn;
+})
